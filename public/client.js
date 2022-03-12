9b0a39afb60790c7b3a282178ebb24012bd5d482
atilla <atilla@atillas-MacBook-Pro.local>
2022-03-12 18:56:05 +0900

    #01

--- public/client.js
+++ public/client.js
@@ -1,8 +1,8 @@
 import * as THREE from 'three'
-import { GLTFLoader } from './jsm/loaders/GLTFLoader.js'
-import { OrbitControls } from './jsm/controls/OrbitControls.js'
-import Stats from './jsm/libs/stats.module.js'
-import { GUI } from './jsm/libs/lil-gui.module.min.js'
+// import { GLTFLoader } from './jsm/loaders/GLTFLoader.js'
+// import { OrbitControls } from './jsm/controls/OrbitControls.js'
+// import Stats from './jsm/libs/stats.module.js'
+// import { GUI } from './jsm/libs/lil-gui.module.min.js'
 
 let camera, scene, renderer, cube;
 
